394f1b24252b544f2549fecc01e8fa44fbdc5413
thenitronos <thenitronos@gmail.com>
2015-10-01 13:16:55 +0000

    ajout de canevas

--- tetris.py
+++ tetris.py
@@ -14,6 +14,7 @@ class Jeu(Tk):
     
     def __init__(self):
         Tk.__init__(self)
-        pass
+        self.canevas = Canvas(master=self, width=largeur_fenetre, height=hauteur_fenetre, bg=gris)
 
+##### Programme principal #####
 Jeu().mainloop()
