eab9d1d3ca895153c24b792a940cf7ddababafbb
thenitronos <thenitronos@gmail.com>
2015-10-08 18:41:10 +0000

    orienté objet pour les grandes fenêtre pricipales

--- classesApp.py
+++ classesApp.py
@@ -10,16 +10,31 @@ from tkinter import *
 from fonctions import *
 from constantes import *
 
-class Jeu(Tk):
-    
-    class Jeu(Tk):
-    
-    def __init__(self, fondPrincipal, geometry, largeur_canevas,
-                 hauteur_canevas, fondCadres):
+
+#Jeu tetris
+###########
+
+class BoutonJeu(Button):
+    "Bouton de fantaisie : vert virant au rouge quand on l'actionne"
+    def __init__(self, **Arguments):
+        Button.__init__(self, bg ="blue", fg ="white", bd =5,
+                        activebackground ="dark blue", activeforeground="white",
+                        font =('Helvetica', 12), **Arguments)
+
+
+class FenetreGrande(Tk):
+
+    def __init__(self, fondPrincipal, geometry):
         Tk.__init__(self)
         self.config(bg=fondPrincipal)
         self.geometry(geometry)
         self.resizable(width=FALSE, height=FALSE)
+    
+class Jeu(FenetreGrande):
+    
+    def __init__(self, fondPrincipal, geometry, largeur_canevas,
+                 hauteur_canevas, fondCadres):
+        FenetreGrande.__init__(self, fondPrincipal, geometry)
 
         self.cadreTetris = Canvas(master=self, width=largeur_canevas,
                                   height=hauteur_canevas, bg=fondCadres)
@@ -31,8 +46,8 @@ class Jeu(Tk):
         self.cadreScores = Canvas(master=self, width="240", height="260", bg=fondCadres)
         self.cadreScores.grid(row=1, column=2, columnspan=2, padx=50, pady=10)
 
-        self.boutonQuitter = Button(master=self, text="Quitter")
+        self.boutonQuitter = BoutonJeu(master=self, text="Quitter")
         self.boutonQuitter.grid(row=2, column=2, pady=30)
 
-        self.boutonPause = Button(master=self, text="Pause")
+        self.boutonPause = BoutonJeu(master=self, text="Pause")
         self.boutonPause.grid(row=2, column=3, pady=30)
